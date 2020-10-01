8fef063229592b01ed28ad8394d56b4fd6226968
Vlad <zhavoronkin.vo@phystech.edu>
2020-10-01 18:31:11 +0300

    hometask-1(2)

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -32,7 +32,8 @@ public class Application {
     }
 
     private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
+        // pretend like we're getting the events from physical world, but here we're going to just
+        // generate some random events
         if (Math.random() < 0.05) return null; // null means end of event stream
         SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
         String objectId = "" + ((int) (10 * Math.random()));
